2cf10401eefd9b3f1946ce01a9928b2c631f2b17
Afiq Amsyar <afiqamsyar80@gmail.com>
2023-03-16 12:53:15 +0800

    add get started button

--- app.vue
+++ app.vue
@@ -61,10 +61,24 @@
               <div class="pt-20 h-full w-full text-lg">
                 <h1 class="text-blue-900 text-4xl font-bold mt-2 mx-2 align-middle">Find Cat Cafe Near You!</h1>
 
-                <p class="my-2 mx-2">Cat cafes typically have a cozy and comfortable atmosphere that's purr-fect for
+                <p class="my-2 mx-2">
+                  Cat cafes typically have a cozy and comfortable atmosphere that's purr-fect for
                   spending quality time with our furry friends. You'll get to experience the soothing presence of the
                   cats, watch them play and snuggle up with them. These cafes often have a wide variety of cat breeds to
-                  choose from, so there's always a cat that fits your preference.</p>
+                  choose from, so there's always a cat that fits your preference.
+                </p>
+
+                <div
+                  class="bg-blue-400 text-white w-48 h-10 rounded-md shadow-lg cursor-pointer mx-2 mt-8 hover:bg-blue-300 grid grid-cols-4 relative">
+                  <NuxtLink class="col-span-3 grid place-items-center">Get Started!</NuxtLink>
+                  <div class="mt-1 rounded-r-md w-48 pr-4 h-6 overflow-hidden absolute flex justify-end">
+                    <svg class="h-10 object-cover " fill="white" xmlns="http://www.w3.org/2000/svg" id="mdi-cat" viewBox="0 0 24 24">
+                      <path
+                        d="M12,8L10.67,8.09C9.81,7.07 7.4,4.5 5,4.5C5,4.5 3.03,7.46 4.96,11.41C4.41,12.24 4.07,12.67 4,13.66L2.07,13.95L2.28,14.93L4.04,14.67L4.18,15.38L2.61,16.32L3.08,17.21L4.53,16.32C5.68,18.76 8.59,20 12,20C15.41,20 18.32,18.76 19.47,16.32L20.92,17.21L21.39,16.32L19.82,15.38L19.96,14.67L21.72,14.93L21.93,13.95L20,13.66C19.93,12.67 19.59,12.24 19.04,11.41C20.97,7.46 19,4.5 19,4.5C16.6,4.5 14.19,7.07 13.33,8.09L12,8M9,11A1,1 0 0,1 10,12A1,1 0 0,1 9,13A1,1 0 0,1 8,12A1,1 0 0,1 9,11M15,11A1,1 0 0,1 16,12A1,1 0 0,1 15,13A1,1 0 0,1 14,12A1,1 0 0,1 15,11M11,14H13L12.3,15.39C12.5,16.03 13.06,16.5 13.75,16.5A1.5,1.5 0 0,0 15.25,15H15.75A2,2 0 0,1 13.75,17C13,17 12.35,16.59 12,16V16H12C11.65,16.59 11,17 10.25,17A2,2 0 0,1 8.25,15H8.75A1.5,1.5 0 0,0 10.25,16.5C10.94,16.5 11.5,16.03 11.7,15.39L11,14Z" />
+                    </svg>
+                  </div>
+
+                </div>
               </div>
             </div>
 
@@ -93,7 +107,9 @@
               Did You Know ?
             </h2>
             <p class="py-6 px-4">
-              Did you know that cats have a special collarbone called the "floating collarbone" or "wishbone" that is not attached to any other bones in their body? This allows cats to fit through narrow spaces and makes them incredibly agile and flexible.
+              Did you know that cats have a special collarbone called the "floating collarbone" or "wishbone" that is not
+              attached to any other bones in their body? This allows cats to fit through narrow spaces and makes them
+              incredibly agile and flexible.
             </p>
 
             <div
