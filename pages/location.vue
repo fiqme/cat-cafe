4e8c72781ef37b1755bb654c0d95acabf7a6477b
Afiq Amsyar <afiqamsyar80@gmail.com>
2023-03-17 17:00:16 +0800

    add sidebar in map

--- pages/location.vue
+++ pages/location.vue
@@ -1,31 +1,38 @@
 <template>
-    <!-- hello -->
-    <div class="h-screen w-screen">
-      <l-map :use-global-leaflet="false" ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
-        <l-tile-layer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          layer-type="base"
-          name="OpenStreetMap"
-        ></l-tile-layer>
-      </l-map>
+    <body>
+        
+    
+    <div class="flex">
+        <div class="h-screen w-full">
+            <l-map :use-global-leaflet="false" ref="map" v-model:zoom="zoom" :center="[3.615342, 107.695053]">
+                <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
+                    name="OpenStreetMap"></l-tile-layer>
+            </l-map>
+            
+        </div>
+        <div class=" bg-white shadow-l-lg h-screen w-52">
+                <p>Hi</p>
+            </div>
+
     </div>
-  </template>
+</body>
+</template>
   
-  <script>
-  import "leaflet/dist/leaflet.css";
-  import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
-  
-  export default {
+<script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default {
     components: {
-      LMap,
-      LTileLayer,
+        LMap,
+        LTileLayer,
     },
     data() {
-      return {
-        zoom: 4,
-      };
+        return {
+            zoom: 6,
+        };
     },
-  };
-  </script>
+};
+</script>
   
-  <style></style>
+<style></style>
