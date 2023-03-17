f4962bb8b4bb4a251e5d8a565bcfcd7c2d1e7a58
Afiq Amsyar <afiqamsyar80@gmail.com>
2023-03-17 15:38:45 +0800

    add leaflet map

--- pages/location.vue
+++ pages/location.vue
@@ -1,20 +1,31 @@
 <template>
-<body>
-    <h1>hi</h1>
-    <NuxtLink to="/">Location</NuxtLink>
-</body>
-</template>
-
-<script>
-export default {
-    setup () {
-        
-
-        return {}
-    }
-}
-</script>
-
-<style scoped>
-
-</style>
+    
+    <div class="h-screen w-screen">
+      <l-map :use-global-leaflet="false" ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+        ></l-tile-layer>
+      </l-map>
+    </div>
+  </template>
+  
+  <script>
+  import "leaflet/dist/leaflet.css";
+  import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+  
+  export default {
+    components: {
+      LMap,
+      LTileLayer,
+    },
+    data() {
+      return {
+        zoom: 4,
+      };
+    },
+  };
+  </script>
+  
+  <style></style>
