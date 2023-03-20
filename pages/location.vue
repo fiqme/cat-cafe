2316899d93a10498202b56d0e5a34e87762d1a53
Afiq Amsyar <afiqamsyar80@gmail.com>
2023-03-20 11:25:27 +0800

    remove sidebar, add map marker

--- pages/location.vue
+++ pages/location.vue
@@ -1,37 +1,41 @@
 <template>
     <body>
-        
-    
-    <div class="flex">
-        <div class="h-screen w-full">
-            <l-map :use-global-leaflet="false" ref="map" v-model:zoom="zoom" :center="[3.615342, 107.695053]">
-                <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
-                    name="OpenStreetMap"></l-tile-layer>
-            </l-map>
-            
-        </div>
-        <div class=" bg-white shadow-l-lg h-screen w-52">
-                <p>Hi</p>
-            </div>
 
-    </div>
-</body>
+        <div>
+            <div class="h-screen w-full">
+                <l-map :use-global-leaflet="false" ref="map" v-model:zoom="zoom" :center="center">
+                    <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
+                        name="OpenStreetMap"></l-tile-layer>
+                        <l-marker :lat-lng="markerLatLng" ></l-marker>
+                        <l-marker :lat-lng="markerLatLng2" ></l-marker>
+                </l-map>
+
+            </div>
+        </div>
+    </body>
 </template>
   
 <script>
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
 
 export default {
     components: {
         LMap,
         LTileLayer,
+        LMarker,
     },
     data() {
         return {
             zoom: 6,
+            center: [3.615342, 107.695053],
+            markerLatLng: [3.1478, 101.6953],
+            markerLatLng2: [3.0333, 101.45],
         };
     },
+    async beforeMount() {
+        
+    }
 };
 </script>
   
